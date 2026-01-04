2fdd72c275917ed60271580c66afb29177ff229e
Luca <geom.lucabuono@gmail.com>
2026-01-04 19:34:08 +0100

    created basic spring-boot app

---
+++ src/main/java/io/github/luca_bn/habit_tracker/HabitTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.github.luca_bn.habit_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/luca_bn/habit_tracker/HabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.luca_bn.habit_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
